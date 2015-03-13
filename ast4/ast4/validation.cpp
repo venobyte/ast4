095b66a6b8cf7b66bb2112f8b22af98705e4a404
venobyte <venobyte@gmail.com>
2015-03-13 12:28:09 -0400

    updated postal code, will start working on the main now.

--- ast4/ast4/validation.cpp
+++ ast4/ast4/validation.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-#define NAME_MAX 30
-#define EMPTY 2
-
 int validateName(string name)
 {
-	int result = 0;
+	int result = VALID;
 	int i = 0;
 	int position = 0;
 	string validName = "abcdefghijklmnopqrstuvwxyz-'. ";
@@ -18,22 +15,22 @@ int validateName(string name)
 	}
 	else if (name.length() > NAME_MAX)
 	{
-		result = 1;
+		result = INVALID;
 	}
 	else if (name.find_first_not_of(validName) != string::npos)
 	{
-		result = 1;
+		result = INVALID;
 	}
 	
-	for (i = 0; i < NAME_MAX && result != 1 && result != EMPTY; i++)
+	for (i = 0; i < NAME_MAX && result != INVALID && result != EMPTY; i++)
 	{
 		position = name.find_first_of("-'. ");
 			
 		if (position != string::npos)
 		{
-			if (name[position + 1] == '-' || name[position + 1] == '\'' || name[position +1] == '.' || name[position +1] == ' ')
+			if (name[position + 1] == '-' || name[position + 1] == '\'' || name[position + 1] == '.' || name[position + 1] == ' ')
 			{
-				result = 1;
+				result = INVALID;
 			}
 			else
 			{
@@ -63,7 +60,66 @@ int validateProvince(string province)
 
 int validatePostal(string postal)
 {
-	return 0;
+	int result = VALID;
+	string firstValidLetter = "ABCEGHJKLMNPRSTVXY";
+	string validLetter = "ABCEGHJKLMNPRSTVWXYZ";
+
+	if (postal == "")
+	{
+		result = EMPTY;
+	}
+	else if (postal.length() > POSTAL_MAX)
+	{
+		result = 1;
+	}
+	else
+	{
+
+		if (firstValidLetter.find(postal[0]) == string::npos)
+		{
+			result = INVALID;
+		}
+		else if (isdigit(postal[1]) == false)
+		{
+			result = INVALID;
+		}
+		else if (validLetter.find(postal[2]) == string::npos)
+		{
+			result = INVALID;
+		}
+
+		if (postal.length() == POSTAL_MAX && result != INVALID)
+		{
+			if (postal[3] == ' ')
+			{
+				postal = postal.substr(4, 3);
+			}
+			else
+			{
+				result = INVALID;
+			}
+		}
+		else if (postal.length() == (POSTAL_MAX - 1) && result != INVALID)
+		{
+			postal = postal.substr(3, 3);
+		}
+
+		if (result != INVALID && isdigit(postal[0]) == false)
+		{
+			result = INVALID;
+		}
+		else if (result != INVALID && validLetter.find(postal[1]) == string::npos)
+		{
+			result = INVALID;
+		}
+		else if (result != INVALID && isdigit(postal[2]) == false)
+		{
+			result = INVALID;
+		}
+
+	}
+
+	return result;
 }
 
 int validatePhone(string phone)
