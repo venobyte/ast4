596271b41f0c3ec7bf8fbc11506b7f587c38168c
venobyte <venobyte@gmail.com>
2015-03-13 11:25:25 -0400

    updated name function, have tested but not extensively but it does seem pretty solid.

--- ast4/ast4/validation.cpp
+++ ast4/ast4/validation.cpp
@@ -2,32 +2,71 @@
 
 using namespace std;
 
+#define NAME_MAX 30
+#define EMPTY 2
+
 int validateName(string name)
 {
+	int result = 0;
+	int i = 0;
+	int position = 0;
+	string validName = "abcdefghijklmnopqrstuvwxyz-'. ";
 
+	if (name == "")
+	{
+		result = EMPTY;
+	}
+	else if (name.length() > NAME_MAX)
+	{
+		result = 1;
+	}
+	else if (name.find_first_not_of(validName) != string::npos)
+	{
+		result = 1;
+	}
+	
+	for (i = 0; i < NAME_MAX && result != 1 && result != EMPTY; i++)
+	{
+		position = name.find_first_of("-'. ");
+			
+		if (position != string::npos)
+		{
+			if (name[position + 1] == '-' || name[position + 1] == '\'' || name[position +1] == '.' || name[position +1] == ' ')
+			{
+				result = 1;
+			}
+			else
+			{
+				name = name.substr(position + 1, name.length()-position);
+			}
+		}
+		
+	}
+	return result;
 }
+
 
 int validateStreet(string street)
 {
-
+	return 0;
 }
 
 int validateCity(string city)
 {
-
+	return 0;
 }
 
 int validateProvince(string province)
 {
-
+	return 0;
 }
 
 int validatePostal(string postal)
 {
-
+	return 0;
 }
 
 int validatePhone(string phone)
 {
-
+	return 0;
 }
