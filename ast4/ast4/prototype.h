08186891023046d989a4453969459ef78f125f88
venobyte <venobyte@gmail.com>
2015-03-13 12:28:30 -0400

    updated definitions

--- ast4/ast4/prototype.h
+++ ast4/ast4/prototype.h
@@ -3,6 +3,14 @@
 #include <string>
 #include <iostream>
 
+#define NAME_MAX 30
+#define VALID 0
+#define INVALID  1
+#define EMPTY 2
+#define POSTAL_MAX 7
+
+using namespace std;
+
 int validateName(string name);
 int validateStreet(string street);
 int validateCity(string city);
