9575a456773d9427898727458c26136db9ab7598
Aric Vogel <vogel.aric@gmail.com>
2015-03-13 16:41:06 -0400

    Added city input in main() and some comments for the others.

--- ast4/ast4/main.cpp
+++ ast4/ast4/main.cpp
@@ -17,11 +17,50 @@ int main(void)
 
 	string input;
 	cout << "Logo\n";
+
+	/* Name */
 	cout << "Enter Name\n:";
 
 	cin >> input;
 
 	validateName(input);
+
+	/* Street */
+
+	/* City */
+	do
+	{
+		// Input
+		cout << "City\n:";
+		cin >> input;
+
+		// Validation
+		if (validateCity(input) != VALID)
+		{
+			char resp = '\0';
+			cout << "City not valid!\n";
+			cout << "Skip <y/n>? ";
+			resp = _getch();
+			if (resp == 'y' || resp == 'Y')
+			{
+				// Skip it
+				break;
+			}
+
+			// Otherwise we loop back
+		}
+		else
+		{
+			// Input is good
+			break;
+		}
+	} while (1);
+
+	/* Province */
+
+	/* Postal Code */
+
+	/* Phone Number */
 
 	return 0;
 }
